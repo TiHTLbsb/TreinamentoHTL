b227f5c8751687c3d2249d02f3eb5b9c8d2f7396
Hiagoperoni <hiagoperonip@gmail.com>
2023-07-03 08:24:32 -0300

    modulo 5 pronto

--- src/Paginas/MOdulo5.jsx
+++ src/Paginas/MOdulo5.jsx
@@ -3,8 +3,10 @@ import Header from '../Componentes/Header';
 import Footer from '../Componentes/Footer';
 import SideBar from '../Componentes/SideBar';
 import '../CSS/Modulos/Modulo5.css';
+import ReactPlayer from "react-player";
 
 function Modulo5 () {
+  const videoUrl = "https://vimeo.com/841231378";
   return (
     <main>
       <Header />
@@ -12,6 +14,7 @@ function Modulo5 () {
       <section className="modulo5Page">
         <section className="modulo5Content">
           <h1 className="modulo5Title">Módulo 5</h1>
+          <ReactPlayer url={videoUrl} controls height="580px" width="95%" marginLeft="2%"/>
         </section>
       </section>
       <Footer />
