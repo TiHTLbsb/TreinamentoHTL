647ad33e0e97589adec3029a456ea1b92a71a7dc
Hiagoperoni <hiagoperonip@gmail.com>
2023-07-03 08:20:10 -0300

    modulo 1 pronto

--- src/Paginas/Modulo1.jsx
+++ src/Paginas/Modulo1.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../Componentes/Header";
 import Footer from "../Componentes/Footer";
 import SideBar from "../Componentes/SideBar";
+import pagina1 from "../Imagens/Modulo 1/SobreNos.jpg";
 import "../CSS/Modulos/Modulo1.css";
 
 function Modulo1() {
@@ -12,7 +13,7 @@ function Modulo1() {
       <section className="modulo1Page">
         <section className="modulo1Content">
           <h1 className="modulo1Title">Módulo 1</h1>
-
+          <img src={pagina1} alt="Sobre Nós" className="paginaMod1"/>
         </section>
       </section>
       <Footer />
